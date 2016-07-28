87d979ba3233e6ed47865125c011650aa9c00944
wadlo <jalen.morgan000@gmail.com>
2016-07-28 13:37:11 -0700

    grid object will draw pixels in a surface.

---
+++ src/grid.py
@@ -0,0 +1,8 @@
+class Grid(object):
+
+	def __init__(self):
+		pass
+	
+	def draw(self, background):
+		pass
+
--- src/pix-fiz.py
+++ src/pix-fiz.py
@@ -4,6 +4,7 @@
 # as the window settings and it runs the main loop of the game.
 
 from menu import Menu
+from grid import Grid
 import pygame as pg
 import sys
 
@@ -17,22 +18,29 @@ class PixFiz(object):
 		self.background = self.tile_bg()
 		self.screen = pg.display.set_mode((600,400))
 		self.clock = pg.time.Clock()
-
-
+		self.menu = Menu()
+		self.grid = Grid()
 		
 
 	def main_loop(self):
 		self.mainLoop = True
 		while self.mainLoop:
+			
+			#key events
 			for event in pg.event.get():
 				if event.type == pg.QUIT:
 					self.mainLoop = False
+			
+			#draw events
 			self.screen.blit(self.background, (0,0))
+			self.menu.draw(self.screen)
+			self.grid.draw(self.screen)
 
-
+			#FPS stuff
 			pg.display.flip()
 			milliseconds = self.clock.tick(FPS)
 			pg.display.set_caption("FPS: " + str(self.clock.get_fps()))
+
 
 	def tile_bg(self): #makes the transparent image to use as a bg.
 		bg = pg.image.load("back.png")
