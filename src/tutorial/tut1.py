61d988b02fc199ee2d730bd5ecd40d6e749fa3ec
alanxoc3 <alanxoc3@gmail.com>
2016-07-28 12:58:27 -0700

    Alan thing.

--- src/tutorial/tut1.py
+++ src/tutorial/tut1.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python2
+
 # Following a tutorial
 import pygame
 import sys
@@ -18,32 +20,50 @@ class KeyState(object):
 	released = 1
 	pressed = 2
 
-class SpamBot:
+class Square(pygame.sprite.Sprite):
+	"""
+	A pixel!
+	"""
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.Surface((64, 64))
+		self.image.fill((12, 89, 200))
+		self.rect = self.image.get_rect()
+
+class SpamBot(pygame.sprite.Sprite):
 	"""
 	Just a test to see how an object can be used to draw.
 	"""
 	def __init__(self, screen, pathname):
-		self.x, self.y = screen.get_size()
-		self.pic = pygame.image.load(pathname)
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(pathname)
+		self.rect = self.image.get_rect()
 
-	def display(self, screen):
-		screen.blit(self.pic, (self.x, self.y))
+		dims = screen.get_size()
+		img_dims = self.image.get_size()
+
+		self.rect.x = dims[0] / 2 - img_dims[0] / 2
+		self.rect.y = dims[1] / 2 - img_dims[1] / 2
 
 	def input(self, arrows):
 		if arrows[Dir.up]:
-			y -= 1
+			self.rect.y -= 1
 		if arrows[Dir.down]:
-		 	y += 1
+		 	self.rect.y += 1
 		if arrows[Dir.left]:
-		 	x -= 1
+		 	self.rect.x -= 1
 		if arrows[Dir.right]:
-			x += 1
+			self.rect.x += 1
 
 def main():
 	pygame.init()
 
 	screen = pygame.display.set_mode(DIM)
 	spammy = SpamBot(screen, "Spambot.png")
+	sprites = pygame.sprite.LayeredUpdates()
+	sprites.add(spammy)
+	sprites.add(Square())
 
 	background = pygame.Surface(screen.get_size())
 	pygame.draw.line(background, (100, 150, 200), (250,250), (300,250), 5)
@@ -70,6 +90,12 @@ def main():
 					arrowHeld[2] = True
 				elif event.key == pygame.K_RIGHT:
 					arrowHeld[3] = True
+				elif event.key == pygame.K_SPACE:
+					inc = 30
+					for sprite in sprites:
+						sprites.change_layer(sprite, inc)
+						inc -= 1
+						
 			elif event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP:
 					arrowHeld[0] = False
@@ -83,11 +109,10 @@ def main():
 		spammy.input(arrowHeld)
 
 		# Draw a line
+		background.fill((255, 255, 255))
+		sprites.draw(background)
 		screen.blit(background, (0, 0))
 
-		spammy.display(screen)
-
-		#screen.fill((255, 255, 255))
 		#pygame.draw.line(screen, (100, 150, 200), (0,0), (100,100), 10)
 		pygame.display.flip()
 		milliseconds = clock.tick(FPS)
