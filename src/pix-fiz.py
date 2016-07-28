733252139eca8caf7ab56f0524cce6cf409a80a6
wadlo <jalen.morgan000@gmail.com>
2016-07-28 11:19:53 -0700

    changed pygame to pg

--- src/pix-fiz.py
+++ src/pix-fiz.py
@@ -1,7 +1,7 @@
 # Here's what we run when we start the game!  It initializes important things such
 # as the window settings and it runs the main loop of the game.
 
-import pygame
+import pygame as pg
 import sys
 
 FPS = 60
@@ -9,28 +9,28 @@ FPS = 60
 class PixFiz(object):
 
 	def __init__(self):
-		pygame.init()
+		pg.init()
 		self.background = self.tile_bg()
-		self.screen = pygame.display.set_mode((600,400))
-		self.clock = pygame.time.Clock()
+		self.screen = pg.display.set_mode((600,400))
+		self.clock = pg.time.Clock()
 		
 
 	def main_loop(self):
 		self.mainLoop = True
 		while self.mainLoop:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
 					self.mainLoop = False
 			self.screen.blit(self.background, (0,0))
-			pygame.display.flip()
+			pg.display.flip()
 			milliseconds = self.clock.tick(FPS)
-			pygame.display.set_caption("FPS: ",  str(self.clock.get_fps()))
+			pg.display.set_caption("FPS: ",  str(self.clock.get_fps()))
 
 	
 
 	def tile_bg(self): #makes the transparent image to use as a bg.
-		bg = pygame.image.load("back.png")
-		trans_back = pygame.Surface((600,400))
+		bg = pg.image.load("back.png")
+		trans_back = pg.Surface((600,400))
 	
 		for i in xrange(0, 600, 32):
 			for j in xrange(0, 400, 32):
