e0b2ae95bf69866fa7fcdf4bc10f12e4d85fd2b4
alanxoc3 <alanxoc3@gmail.com>
2016-07-29 18:35:08 -0700

    work on zoom

--- src/grid.py
+++ src/grid.py
@@ -5,7 +5,7 @@ class Grid(object):
 	def __init__(self):
 		self.res = (600,400) # The dimension of the surface
 		self.pix = (100,100) # Number of pixels on screen/board/canvas
-		self.offset = [0, 0] # Offset on surface that things are drawn.
+		self.zoom_off = [0.0, 0.0] # The zoomed offset
 		self.grid = pg.Surface(self.res)
 		self.zoom_factor = 1.0
 
@@ -46,35 +46,46 @@ class Grid(object):
 			self.pix_matrix[y][x] = 1
 			self.redraw_canvas()
 
-	def zoom_in(self, pos, amount=1):
-		self.zoom_factor *= 1.1
+	def zoom(self, pos, isZoomIn):
+		self.before_zoom = self.zoom_factor
+		zoom = 1.1
+
+		if isZoomIn == True:
+			self.zoom_factor *= zoom
+		else:
+			self.zoom_factor /= zoom
+
 		if self.zoom_factor > globe.MAX_ZOOM_IN:
 			self.zoom_factor = globe.MAX_ZOOM_IN
-		self.redraw_canvas()
-		print "ZOOM IN", self.zoom_factor
-
-	def zoom_out(self, pos, amount=1):
-		before_zoom = self.zoom_factor
-
-		# Change the zoom
-		self.zoom_factor /= 1.1
 		if self.zoom_factor < globe.MAX_ZOOM_OUT:
 			self.zoom_factor = globe.MAX_ZOOM_OUT
 
-		self.offset[0] = int(self.offset[0] + pos[0] * (before_zoom - self.zoom_factor))
-		self.offset[1] = int(self.offset[1] + pos[1] * (before_zoom - self.zoom_factor))
+		scalechange = self.zoom_factor - self.before_zoom
+		if scalechange != 0:
+			#pan_x = -(pos[0] / (self.zoom_factor*zoom) - pos[0]/self.zoom_factor)
+			#pan_y = -(pos[1] / (self.zoom_factor*zoom) - pos[1]/self.zoom_factor)
+			pan_x = -(pos[0] * scalechange)
+			pan_y = -(pos[1] * scalechange)
+			print "X: ", pan_x, " Y: ", pan_y
+			self.pan((pan_x, pan_y))
 
 		self.redraw_canvas()
-		print "ZOOM out", self.zoom_factor
+
+	def zoom_in(self, pos, amount=1):
+		self.zoom(pos, True)
+
+	def zoom_out(self, pos, amount=1):
+		self.zoom(pos, False)
 
 	def pan(self, delta):
-		self.offset[0] += delta[0]
-		self.offset[1] += delta[1]
+		self.zoom_off[0] += delta[0]
+		self.zoom_off[1] += delta[1]
+		print "Zoom Off: ", self.zoom_off
 		self.redraw_canvas()
 
 	def pos_to_pix(self, pos):
-		x = (pos[0] - self.offset[0]) / self.pix_dim[0]
-		y = (pos[1] - self.offset[1]) / self.pix_dim[1]
+		x = int((pos[0] - self.zoom_off[0]) / self.pix_dim[0])
+		y = int((pos[1] - self.zoom_off[1]) / self.pix_dim[1])
 		if x >= self.pix[0] or x < 0:
 			x = -1
 		if y >= self.pix[1] or x < 0:
@@ -88,10 +99,10 @@ class Grid(object):
 			for col in range(0,self.pix[0]):
 				val = self.pix_matrix[row][col]
 				if val == 1:
-					pos = (col * self.pix_dim[0] + self.offset[0], row * self.pix_dim[1] + self.offset[1])
+					pos = (col * self.pix_dim[0] + self.zoom_off[0], row * self.pix_dim[1] + self.zoom_off[1])
 					r = pg.Rect(pos, self.pix_dim)
-					#pg.draw.rect(self.grid, colors[0], r)
-					self.grid.fill(colors[0], r)
+					pg.draw.rect(self.grid, colors[0], r)
+					#self.grid.fill(colors[0], r)
 
 	def drawLines(self):
 		if self.zoom_factor < globe.GRID_ZOOM_GONE:
@@ -103,13 +114,13 @@ class Grid(object):
 		incs = (self.pix_dim[0], self.pix_dim[1])
 
 		for y in xrange(0, lens[1] + 1, incs[1]): # Horizontal lines
-			tup1 = (0 + self.offset[0]          , y + self.offset[1])
-			tup2 = (lens[0] + self.offset[0], y + self.offset[1])
+			tup1 = (0 + self.zoom_off[0]          , y + self.zoom_off[1])
+			tup2 = (lens[0] + self.zoom_off[0], y + self.zoom_off[1])
 			pg.draw.line(self.grid, color, tup1, tup2, width)
 
 		for x in xrange(0, lens[0] + 1, incs[0]): # Vertical lines
-			tup1 = (x + self.offset[0], 0 + self.offset[1]          )
-			tup2 = (x + self.offset[0], lens[1] + self.offset[1])
+			tup1 = (x + self.zoom_off[0], 0 + self.zoom_off[1]          )
+			tup2 = (x + self.zoom_off[0], lens[1] + self.zoom_off[1])
 			pg.draw.line(self.grid, color, tup1, tup2, width)
 
 	def draw(self, screen):
